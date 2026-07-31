e465cb4bb0344c640839245a182c9e1a21c0057a
lfmtz <luis.martinez.autos@gmail.com>
2026-07-31 15:08:30 -0600

    Update desktop shortcut script for OneDrive path

--- scratch/create_shortcut.ps1
+++ scratch/create_shortcut.ps1
@@ -1,5 +1,9 @@
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut([System.IO.Path]::Combine([Environment]::GetFolderPath('Desktop'), "Administrador de Autos.lnk"))
+$DesktopPath = [System.IO.Path]::Combine([Environment]::GetFolderPath('UserProfile'), "OneDrive", "Desktop")
+if (-not (Test-Path $DesktopPath)) {
+    $DesktopPath = [Environment]::GetFolderPath('Desktop')
+}
+$Shortcut = $WshShell.CreateShortcut([System.IO.Path]::Combine($DesktopPath, "Administrador de Autos.lnk"))
 $Shortcut.TargetPath = "wscript.exe"
 $Shortcut.Arguments = "`"c:\Users\luism\Documents\catalogo-seminuevos\Iniciar-Servidor-Silencioso.vbs`""
 $Shortcut.IconLocation = "shell32.dll, 74"
